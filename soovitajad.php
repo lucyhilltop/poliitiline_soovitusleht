cca0cbcc7d50ca46a80c0b7a5929ce443dee57b3
karlerki1 <karlerki@ut.ee>
2015-03-17 11:29:07 +0200

    Andmebaasiasisoovitajatele

--- soovitajad.php
+++ soovitajad.php
@@ -8,6 +8,7 @@
 <style type="text/css"></style>
 </head>
 <body>
+
 <div id ='konteiner'>	
 	<div id='pais'>
 			<p id='pealkiri'>Poliitiline soovitusleht</p>
@@ -24,6 +25,36 @@
             <input id="searchbox" type="text" placeholder="Search..." required>
             <input id="searchbutton" type="submit" value="">
 	</form>
+	
+	<?php
+	// Connect to database server
+	mysql_connect("ejx5shwlyf.database.windows.net", "server", "Parool11") or die (mysql_error ());
+
+	// Select database
+	mysql_select_db("andmebaas") or die(mysql_error());
+
+	// SQL query
+	$strSQL = "SELECT * FROM Kandidaadid";
+
+	// Execute the query (the recordset $rs contains the result)
+	$rs = mysql_query($strSQL);
+	
+	
+	// Loop the recordset $rs
+	while($row = mysql_fetch_array($rs)) {
+
+	  
+	  
+	  
+	  echo $row['number'];
+
+
+	  }
+
+	// Close the database connection
+	mysql_close();
+	?>
+	
 	
 	<table id="soovitajad">
 		<thead>
