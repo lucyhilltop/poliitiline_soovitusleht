905c9fb5007aec743bea9be905dbb1cb0f4633a7
karlerki1 <karlerki@ut.ee>
2015-03-24 18:26:00 +0200

    m

--- kandidaadid.php
+++ kandidaadid.php
@@ -54,6 +54,8 @@
 	$sql = "SELECT * FROM Soovitajad";
 	$result = mysqli_query($conn, $sql);
 	
+	echo $mysqli_result->num_rows;
+	
 	if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
