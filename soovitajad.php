793ccab8719f7fd21173c085db2c21e1b325a840
silverwithp <silverpajumae@hotmail.com>
2015-03-24 11:42:31 +0200

    Update soovitajad.php

--- soovitajad.php
+++ soovitajad.php
@@ -35,6 +35,7 @@
     echo "<h2>Alustan Connectimist</h2>";
     $conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
 	echo "<h2>PEALE CONNECTIMIST</h2>";
+	echo "<h2>$conn</h2>";
     if($conn == false){
     	echo "<h2>Error</h2>";
         die(print_r(sqlsrv_errors()));
