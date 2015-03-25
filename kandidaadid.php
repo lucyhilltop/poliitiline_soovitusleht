fdb20a2fd7e52e8752ecfc24b8118c50774e025e
karlerki1 <karlerki@ut.ee>
2015-03-25 11:57:30 +0200

    d

--- kandidaadid.php
+++ kandidaadid.php
@@ -19,35 +19,15 @@ include ("header.php");
     $pwd = "Parool11";
     $db = "andmebaas";
     $conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
-
     if($conn == false){
     	echo "<h2>Error</h2>";
         die(print_r(sqlsrv_errors()));
     }
    
-    echo "<h2>Connectimisega on korras</h2>"
-	/*
-	try{
-    $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE registration_tbl(
-                id INT NOT NULL AUTO_INCREMENT, 
-                PRIMARY KEY(id),
-                name VARCHAR(30),
-                email VARCHAR(30),
-                date DATE)";
-    $conn->query($sql);
-	}
-	catch(Exception $e){
-		die(print_r($e));
-	}
-	echo "<h3>Table created.</h3>";
-	*/
-	
+    echo "<h2>Connectimisega on korras</h2>";
 	$sql = "SELECT * FROM Kandidaadid";
 	$result = mysqli_query($conn, $sql);
 	
-
 	if (mysql_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
@@ -56,8 +36,6 @@ include ("header.php");
 	} else {
 		echo "0 results";
 	}
-	
-
 	
 	mysql_close($conn);
 			
