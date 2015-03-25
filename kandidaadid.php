b068ac834f14fb0f1297840931d95d0f4ebd11db
karlerki1 <karlerki@ut.ee>
2015-03-25 11:49:35 +0200

    j

--- kandidaadid.php
+++ kandidaadid.php
@@ -18,7 +18,7 @@ include ("header.php");
     $user = "server@ejx5shwlyf";
     $pwd = "Parool11";
     $db = "andmebaas";
-    $conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+    //$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
 
     if($conn == false){
     	echo "<h2>Error</h2>";
