665d067ecd467e5c0d5085abc9d4b390ba18b1cc
karlerki1 <karlerki@ut.ee>
2015-03-31 13:32:50 +0300

    Muudatus kandides

--- kandidaadid.php
+++ kandidaadid.php
@@ -7,60 +7,31 @@ include ("dbfun.php");
             <input id="searchbox" type="text" placeholder="Search..." required>
             <input id="searchbutton" type="submit" value="">
 	</form>
+	
 	<table id="soovitajad">
 		<thead>
 			<tr>
 				<th>Kandidaadid</th>
 			</tr>
 		</thead>
-		
-	<?php
-	
-	$conn = connect();
-	$sql = "SELECT * FROM Kandidaadid";
-	$stmt=sqlsrv_query($conn, $sql);
-	
-	while( $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
-      echo $row['nimi']."<br />";
-	}
-
-	
-	
-	
-	?>
-	
-		
 		<tbody>
-			<tr>
-				<td><a href="kandidaadid.php" class="button">nr 100 Silver Pajumäe</a></td>
-			</tr>
-			<tr>
-				<td><a href="kandidaadid.php" class="button">nr 101 Liis Mäeots</a></td>
-			</tr>
-			<tr>
-				<td><a href="kandidaadid.php" class="button">nr 102 Karl Erki Jürgen</a></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
+		<?php
+	
+			$conn = connect();
+			$sql = "SELECT * FROM Kandidaadid";
+			$stmt=sqlsrv_query($conn, $sql);
+			
+			$miturida=0;
+			while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+			  echo '<tr><td><a href="kandidaadid.php" class="button">'."nr"." ".$row['number']."  ".$row['nimi'].'</a></td></tr>';
+			  $miturida++;
+			}
+			//kui on vähe liikmeid, siis kuvatakse tyhju kaste. Lihtsalt visuaalne asi.
+			while($miturida<13){
+				echo '<tr><td><a href="kandidaadid.php" class="button"></a></td></tr>';
+				$miturida++;
+			}
+		?>
 		</tbody>
 	</table>
 </div>
