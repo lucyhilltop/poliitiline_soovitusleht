ad777d872e2ef89057ce8760a188e46548fac6ac
karlerki1 <karlerki@ut.ee>
2015-03-31 13:30:11 +0300

    Soovitajad ja Kandidaadid esitavad andmeid õigesti

--- soovitajad.php
+++ soovitajad.php
@@ -1,37 +1,12 @@
 ﻿<?php
 include ("header.php");
+include ("dbfun.php");
 ?>
 
 	<form action="#">
             <input id="searchbox" type="text" placeholder="Search..." required>
             <input id="searchbutton" type="submit" value="">
 	</form>
-	
-	<?php
-	$conn = connect();
-	
-	$sql = "SELECT * FROM Soovitajad";
-	echo "<h2>sql valitud</h2>";
-	try {
-		echo "Olen try sees";
-		$resultSet = $conn->query("SELECT * FROM Kandidaadid");
-		echo "Väljun try'st";
-	}
-	catch(Exception $e) {
-       	echo "<h2>$e</h2>";
-    }
-	echo "<h2>result valitud</h2>";
-	if ($resultSet->num_rows > 0) {
-	// output data of each row
-		while($row = $resultSet->fetch_assoc()) {
-			echo "id: " . $row["id"]. " - Name: " . $row["nimi"]."<br>";
-				}
-		} else {
-			echo "<h2>0 tulemust</h2>";
-		}
-		
-	?>
-	
 	
 	<table id="soovitajad">
 		<thead>
@@ -40,36 +15,23 @@ include ("header.php");
 			</tr>
 		</thead>
 		<tbody>
-			<tr>
-				<td><a href="kandidaadid.html" class="button">Silver Pajumäe</a></td>
-			</tr>
-			<tr>
-				<td><a href="kandidaadid.html" class="button">Liis Mäeots</a></td>
-			</tr>
-			<tr>
-				<td><a href="kandidaadid.html" class="button">Karl Erki Jürgen</a></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
+		<?php
+	
+			$conn = connect();
+			$sql = "SELECT * FROM Soovitajad";
+			$stmt=sqlsrv_query($conn, $sql);
+			
+			$miturida=0;
+			while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+			  echo '<tr><td><a href="kandidaadid.php" class="button">'.$row['nimi'].'</a></td></tr>';
+			  $miturida++;
+			}
+			//kui on vähe liikmeid, siis kuvatakse tyhju kaste. Lihtsalt visuaalne asi.
+			while($miturida<13){
+				echo '<tr><td><a href="kandidaadid.php" class="button"></a></td></tr>';
+				$miturida++;
+			}
+			?>
 		</tbody>
 	</table>
 </div>
