3c7058715a64be37e678e896a1d9e2cbf9096f17
karlerki1 <karlerki@ut.ee>
2015-04-02 12:31:05 +0300

    Kutse lehele ankeet

--- kutse.php
+++ kutse.php
@@ -1,7 +1,28 @@
 <?php
 include ("header.php");
+include ("dbfun.php");
 ?>
 
+
+<div id="invite">
+    
+	<form>
+			
+		<label>Nimi</label>
+		<input name="name" placeholder="Kandidaadi nimi">
+				
+		<label>Number</label>
+		<input name="email" type="email" placeholder="Kandidaadi number">
+		
+		<label>Erakond</label>
+		<input name="email" type="email" placeholder="Kandidaadi erakond">
+		
+		<label>Kirjeldus</label>
+		<textarea name="message" placeholder="Lühike ülevaade kandidaadist"></textarea>
+				
+		<input id="submit" name="submit" type="submit" value="Submit">
+			
+	</form>
 </div>
 <?php
 include ("footer.php");
