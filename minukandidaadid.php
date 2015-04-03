8b0eb2ff2db508c26918063faec21ed5071192d6
silverwithp <silverpajumae@hotmail.com>
2015-04-03 20:32:08 +0300

    Update minukandidaadid.php

--- minukandidaadid.php
+++ minukandidaadid.php
@@ -7,9 +7,10 @@ include ("header.php");
   the JavaScript SDK to present a graphical Login button that triggers
   the FB.login() function when clicked.
 -->
-
+<CENTER>
 <fb:login-button scope="public_profile,email" data-size="xlarge" onlogin="checkLoginState();">
 </fb:login-button>
+</CENTER>
 <script src="js/facebook.js"></script>
 
 <div id="status">
