208257b71fd768dc624332b886d8fb368cee871b
karlerki1 <karlerki@ut.ee>
2015-04-03 12:26:52 +0300

    AJAXeerimine peaaegu valmis

--- ajax/data.php
+++ ajax/data.php
@@ -1,11 +1,59 @@
 <?php
+//XMLitakse
+header('Content-Type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' . "\n";
 
+echo "<response>\n";
 if (isset($_POST["ID"])=== true && empty($_POST["ID"])=== false) {
 	require '../funktsioonid/dbfun.php';
 	$data=getOneKandi($_POST["ID"] );
 	echo $data["nimi"];
-	
+	echo $data["number"];
 }
+else{
+	echo "EII";
+}
+echo "</response>\n";
+?>
 
 
+<?PHP
+/*
+// Original PHP code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
+
+class xmlResponse
+{
+
+  function start()
+  {
+    header('Content-Type: text/xml');
+    echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' . "\n";
+    echo "<response>\n";
+  }
+
+  function command($method, $params=array(), $encoded=array())
+  {
+    echo "  <command method=\"$method\">\n";
+    if($params) {
+      foreach($params as $key => $val) {
+        echo "    <$key>" . htmlspecialchars($val) . "</$key>\n";
+      }
+    }
+    if($encoded) {
+      foreach($encoded as $key => $val) {
+        echo "    <$key><![CDATA[$val]]></$key>\n";
+      }
+    }
+    echo "  </command>\n";
+  }
+
+  function end()
+  {
+    echo "</response>\n";
+    exit;
+  }
+
+}
+*/
 ?>
--- js/konteinerism.js
+++ js/konteinerism.js
@@ -1,14 +1,78 @@
+//siin failis tegeletakse kandidaatide info kuvamise asjadega, mis on js
+
+var xmlHttp = createXmlHttpRequestObject();
+//alert("js tootab");
+
+
 function getData(id) {
-		$.post('ajax/data.php', {ID:id}, function(data){
-		showContainer(data);
+		if (xmlHttp.readyState==0 || xmlHttp.readyState==4){
+			xmlHttp.open("POST", 'ajax/data.php?ID='+id, true);
+			xmlHttp.onreadystatechange = handleServerResponse;
+			xmlHttp.send();
+		}else{
+			setTimeout('process()',1000);
+		}
 		
-		});
+		
+		
+		
+		//$.post('ajax/data.php', {ID:id}, function(data){
+		//showContainer(data);
+		
 	}
 	
-function showContainer(data) {
-		document.getElementById("nurkkonteiner").style.visibility="visible"
-		alert((data));
-		//muudame konteineri andmed oigeks
-		document.getElementById("nimi").innerHTML = data;
-
+	
+function handleServerResponse() {
+	if (xmlHttp.readyState==4){
+		if(xmlHttp.status==200){
+			xmlResponse=xmlHttp.responseXML;
+			xmlDocumentElement=xmlResponse.documentElement;
+			message= xmlDocumentElement.firstChild.data;
+			
+			
+			document.getElementById("nimi").innerHTML = message;
+			
+			alert(message);
+			
+			document.getElementById("nurkkonteiner").style.visibility="visible"
+			
+			setTimeout('process()',1000);
+		}else{
+			alert("22 midagi laks valesti...");
+		}
+			//alert((data));
+			//muudame konteineri andmed oigeks
+			/*
+			document.getElementById("nimi").innerHTML = data;
+			document.getElementById("number").innerHTML = data;
+			document.getElementById("erakond").innerHTML = data;
+			document.getElementById("kirjeldus").innerHTML = data;
+			*/
+		}
 	}
+
+	
+//AJAXi jama
+function createXmlHttpRequestObject(){
+	var xmlHttp;
+	if(window.ActiveXObject){
+		try{
+			xmlHttp= new ActiveXObject("Microsoft.XMLHTTP");
+		}catch(e){
+			xmlHttp=false;
+		}
+	}else{
+		try{
+			xmlHttp= new XMLHttpRequest();
+		}catch(e){
+			xmlHttp=false;
+		}
+	}
+	
+	if (!xmlHttp){
+		alert("Midagi on halvasti!!");
+	}else{
+		return xmlHttp;
+	}
+	
+}
