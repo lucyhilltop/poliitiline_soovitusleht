a59994384746b3e680d6958047b97c043191e582
Lucy Hilltop <liisupzz@gmail.com>
2015-04-04 23:37:00 +0300

    Loodan, et mingi fail välja ei jäänud. Pirukas VÕIKS tehniliselt töötada, aga pole kindel, ei saanud oma arvutis andmebaasiga testida. Here goes nothing.

--- funktsioonid/dbfun.php
+++ funktsioonid/dbfun.php
@@ -2,99 +2,93 @@
 //andmebaassiga yhendamine
 function connect()
 {
-	$server = "tcp:ejx5shwlyf.database.windows.net, 1433";
+    $server = "tcp:ejx5shwlyf.database.windows.net, 1433";
     $user = "server@ejx5shwlyf";
     $pwd = "Parool11";
     $db = "andmebaas";
-	try{
-		$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db, "CharacterSet" => "UTF-8"));
-	}
-	catch(Exception $e){
-		die(print_r($e));
-	}
-	return $conn;
+    try{
+        $conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db, "CharacterSet" => "UTF-8"));
+    }
+    catch(Exception $e){
+        die(print_r($e));
+    }
+    return $conn;
 }
-
 // eitoota korralikult
 function getAll($table)
 {
-	$conn = connect();
-	$sql = "SELECT * FROM ".$table;
-	return $stmt =sqlsrv_query($conn, $sql);
-	
-	
-}
+    $conn = connect();
+    $sql = "SELECT * FROM ".$table;
+    return $stmt =sqlsrv_query($conn, $sql);
 
+
+}
 // votab koik info kandidaadaadi kohta
 function getOneKandi($id)
 {
-	$conn = connect();
-	$sql = "SELECT Kandidaadid.nimi, Kandidaadid.number, Erakonnad.ErakonnaNimi, Kandidaadid.kirjeldus
+    $conn = connect();
+    $sql = "SELECT Kandidaadid.nimi, Kandidaadid.number, Erakonnad.ErakonnaNimi, Kandidaadid.kirjeldus
 	FROM Kandidaadid
 	INNER JOIN Erakonnad
 	ON Kandidaadid.erakond=Erakonnad.ID
 	WHERE Kandidaadid.ID=".$id;
-	
-	
-	$stmt=sqlsrv_query($conn, $sql);
 
-	while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
-		$data=[
-		"nimi"  => $row["nimi"],
-		"number" =>$row['number'],
-		"erakond" =>$row['ErakonnaNimi'],
-		"kirjeldus" =>$row['kirjeldus']
-		];
-		
-	}
-	return $data;
 
+    $stmt=sqlsrv_query($conn, $sql);
+    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+        $data=[
+            "nimi"  => $row["nimi"],
+            "number" =>$row['number'],
+            "erakond" =>$row['ErakonnaNimi'],
+            "kirjeldus" =>$row['kirjeldus']
+        ];
+
+    }
+    return $data;
 }
-
 // votab koik info soovitaja kohta
 function getOneSoovit($id)
 {
-	$conn = connect();
-	$sql = "SELECT Soovitajad.nimi, COUNT(Toetus.SoovitajaID) AS toetanud FROM Toetus
+    $conn = connect();
+    $sql = "SELECT Soovitajad.nimi, COUNT(Toetus.SoovitajaID) AS toetanud FROM Toetus
 	LEFT JOIN Soovitajad
 	ON Soovitajad.ID=Toetus.SoovitajaID
 	WHERE Soovitajad.ID=".$id."
 	GROUP BY nimi";
-	
-	
-	$stmt=sqlsrv_query($conn, $sql);
 
-	while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
-		$data=[
-		"nimi"  => $row["nimi"],
-		"toetanud" =>$row['toetanud'],
-		];
-		
-	}
-	return $data;
 
+    $stmt=sqlsrv_query($conn, $sql);
+    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+        $data=[
+            "nimi"  => $row["nimi"],
+            "toetanud" =>$row['toetanud'],
+        ];
+
+    }
+    return $data;
 }
-
 // votab top 3e 
 function getTop()
 {
-	$conn = connect();
-	$sql = "SELECT TOP 3 Kandidaadid.nimi, COUNT(Toetus.KandidaadiID) AS Toetatud FROM Toetus
+    $conn = connect();
+    $sql = "SELECT TOP 3 Kandidaadid.nimi, COUNT(Toetus.KandidaadiID) AS Toetatud FROM Toetus
 	LEFT JOIN Kandidaadid
 	ON Toetus.KandidaadiID=Kandidaadid.ID
 	GROUP BY nimi
 	ORDER BY Toetatud DESC";
-	
-	$stmt=sqlsrv_query($conn, $sql);
-	$data=array();
-	
-	while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
-		array_push($data, $row["nimi"]);
-		
-	}
-	return $data;
 
+    $stmt=sqlsrv_query($conn, $sql);
+    $data=array();
+
+    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+        array_push($data, $row["nimi"]);
+
+    }
+    return $data;
 }
-
+function getPie () {
+    $conn = connect();
+    $sql = "SELECT * FROM pirukas";
+    return $stmt = sqlsrv_query($conn, $sql);
+}
 ?>
-
--- index.php
+++ index.php
@@ -22,18 +22,22 @@ include ("funktsioonid/dbfun.php");
 			?>
 			</ol>
 	</div>
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script src="pirukas.js">
-    </script>
+    <script type="text/javascript" src="countdown.js"></script>
 	<div id="graaf">
 		<h3>Aega järgmiste valimisteni:</h3>
 			<span id="countdown">21 päeva, 2 tundi, 5 minutit, 14 sekundit</span>
         <script src="countdown.js"></script>
+
+
 		<h3>Erakondade populaarsus:</h3>
-        <div id="donutchart" style="height: 400px; width: 500px;"></div>
+        <canvas id="pirukas" width="400" height="400"></canvas>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+        <script src="pirukas.js"></script>
 	</div>
-	
+
 	<div class="clear"></div>
+
 </div>
 <?php
 include ("footer.php");
