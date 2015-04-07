615f955a0a3c39ca91cc84e1b436c37b383eaf66
silverwithp <silverpajumae@hotmail.com>
2015-04-07 11:45:23 +0300

    Update kandidaadid.php

--- kandidaadid.php
+++ kandidaadid.php
@@ -20,7 +20,8 @@ include ("funktsioonid/dbfun.php");
 			
 			$miturida=0;
 			while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
-				  echo '<div id="osa1"><h2><a href="#" style="text-decoration:none"
+				$KandiID=thid.id;
+				  echo '<div id="osa1"><h2><a href="#{$KandiID}" style="text-decoration:none"
 					id='.$row["ID"].'
 					onClick="$kandiID=this.id;getDataKandi($kandiID);"  
 				  >'."nr"." ".$row['number']."  ".$row['nimi'].'</a></h2></div>';
