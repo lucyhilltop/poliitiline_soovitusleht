539f9cdfee383196d6d4a19704f19c15e73b8396
silverwithp <silverpajumae@hotmail.com>
2015-04-07 12:25:29 +0300

    Test #

--- kandidaadid.php
+++ kandidaadid.php
@@ -20,10 +20,10 @@ include ("funktsioonid/dbfun.php");
 			
 			$miturida=0;
 			while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
-				  echo '<div id="osa1"><h2><a href="#" style="text-decoration:none"
+				  echo '<div id="osa1"><h2><a href="#">test</a> style="text-decoration:none"
 					id='.$row["ID"].'
 					onClick="$kandiID=this.id;getDataKandi($kandiID);"  
-				  >'."nr"." ".$row['number']."  ".$row['nimi'].'</a></h2></div>';
+				  >'."nr"." ".$row['number']."  ".$row['nimi'].'</h2></div>';
 				  $miturida++;
 				  
 			}
