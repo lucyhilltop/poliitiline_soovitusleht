36f68119202bc47e10c05f9fda70bfe1193be3a7
silverwithp <silverpajumae@hotmail.com>
2015-04-07 12:26:44 +0300

    Tagasi

--- kandidaadid.php
+++ kandidaadid.php
@@ -20,7 +20,7 @@ include ("funktsioonid/dbfun.php");
 			
 			$miturida=0;
 			while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
-				  echo '<div id="osa1"><h2><a href="#">test style="text-decoration:none"
+				  echo '<div id="osa1"><h2><a href="#" test style="text-decoration:none"
 					id='.$row["ID"].'
 					onClick="$kandiID=this.id;getDataKandi($kandiID);"  
 				  >'."nr"." ".$row['number']."  ".$row['nimi'].'</a></h2></div>';
