7eef24d69b5bddf2bfdcf79f594746f368b5f6dc
silverwithp <silverpajumae@hotmail.com>
2015-04-14 10:35:48 +0300

    menüü change

--- header.php
+++ header.php
@@ -23,8 +23,8 @@
 			<li><a href="index.php" class="button">Avaleht</a></li>
 			<li><a href="soovitajad.php" class="button">Soovitajad</a></li>
 			<li><a href="kandidaadid.php" class="button">Kandidaadid</a></li>
-			<li><a href="kutse.php" class="button">Kutse</a></li>
-			<li><a href="minukandidaadid.php" class="button">Minu kandidaadid</a></li>
+			<li><a href="kutse.php" class="button">Kandidaadi lisamine</a></li>
+			<li><a href="minukandidaadid.php" class="button">Minu seaded</a></li>
 		</div>
     </div>
 	</div>
