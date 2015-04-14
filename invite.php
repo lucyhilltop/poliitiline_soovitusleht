8c92658652416f69328c19df2d17148c606bd455
lucyhilltop <lucyhilltop@users.noreply.github.com>
2015-04-14 11:06:05 +0300

    nupp peaks suurem nüüd veits olema

--- invite.php
+++ invite.php
@@ -9,4 +9,4 @@ include ("header.php");
   js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<div class="fb-send" data-href="http://valimissoovitus.azurewebsites.net/" data-colorscheme="light"></div>
+<div class="fb-send" data-href="http://valimissoovitus.azurewebsites.net/" data-colorscheme="dark" data-width="165"></div>
