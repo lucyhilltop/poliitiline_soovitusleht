ddd2155c876aabb5e47c1b14f0ff9b5b0f3a3a5a
lucyhilltop <lucyhilltop@users.noreply.github.com>
2015-04-23 00:13:08 +0300

    Update kandidaadid.php

--- kandidaadid.php
+++ kandidaadid.php
@@ -18,7 +18,8 @@ include ("funktsioonid/dbfun.php");
 					id='.$row["ID"].'
 					onClick="$kandiID=this.id;getDataKandi($kandiID);"
 					test style="text-decoration:none"
-					href="#id"
+					href="#"
+					window.history.pushState(“”, “Kandidaadid”, “/this.id”);
 				  >'."nr"." ".$row['number']."  ".$row['nimi'].'</a></h2></div>';
 				  $miturida++;
 				  
