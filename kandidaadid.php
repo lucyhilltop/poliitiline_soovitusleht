e98881d91c5ade888f10d6152197df026e4c6606
silverwithp <silverpajumae@hotmail.com>
2015-04-26 21:10:02 +0300

    Lisan funktsiooni

--- kandidaadid.php
+++ kandidaadid.php
@@ -5,7 +5,12 @@ include ("funktsioonid/dbfun.php");
 	
 	<div id="navigation">
 
-		<?php		
+		<?php	
+			function setInterval($f, $milliseconds)	{
+				 $seconds=(int)$milliseconds/1000;
+    				while(true) {
+					 $f();
+        				sleep($seconds);}}
 			$conn = connect();
 			$sql = "SELECT * FROM Kandidaadid";
 			$stmt=sqlsrv_query($conn, $sql);
