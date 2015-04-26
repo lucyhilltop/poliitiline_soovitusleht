59d9e48fd06610c3395412fcfe1a7a9621816844
Lucy Hilltop <liisupzz@gmail.com>
2015-04-26 23:01:37 +0300

    I am very much disliking everything right now

--- soovitajad.php
+++ soovitajad.php
@@ -5,7 +5,28 @@ include ("funktsioonid/dbfun.php");
 ?>
 	
 	<div id="navigation">
-	
+	$(function(){
+
+  // Bind an event to window.onhashchange that, when the hash changes, gets the
+  // hash and adds the class "selected" to any matching nav link.
+  $(window).hashchange( function(){
+    var hash = location.hash;
+
+    // Set the page title based on the hash.
+    document.title = 'The hash is ' + ( hash.replace( /^#/, '' ) || 'blank' ) + '.';
+
+    // Iterate over all nav links, setting the "selected" class as-appropriate.
+    $('#nav a').each(function(){
+      var that = $(this);
+      that[ that.attr( 'href' ) === hash ? 'addClass' : 'removeClass' ]( 'selected' );
+    });
+  })
+
+  // Since the event is only triggered when the hash changes, we need to trigger
+  // the event now, to handle the hash the page may have loaded with.
+  $(window).hashchange();
+
+});
 		<?php
 		
 			$conn = connect();
@@ -19,8 +40,9 @@ include ("funktsioonid/dbfun.php");
 					onClick="getDataSoov(this.id)"
 				  >'.$row['nimi'].'</a></h2></div>';
 				  $miturida++;
+
 			}
-            trellid();
+
 			//kui on vähe liikmeid, siis kuvatakse tyhju kaste. Lihtsalt visuaalne asi.
 			while($miturida<13){
 				echo '<tr><td><a href="kandidaadid.php" class="button"></a></td></tr>';
