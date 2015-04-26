a7ebba8126a10ce47be188769d0262c2cf69d7ef
Lucy Hilltop <liisupzz@gmail.com>
2015-04-26 23:21:42 +0300

    no more text

--- header.php
+++ header.php
@@ -7,28 +7,7 @@
 		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 		<link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
 		<style type="text/css"></style>
-        $(function(){
-
-        // Bind an event to window.onhashchange that, when the hash changes, gets the
-        // hash and adds the class "selected" to any matching nav link.
-        $(window).hashchange( function(){
-        var hash = location.hash;
-
-        // Set the page title based on the hash.
-        document.title = 'The hash is ' + ( hash.replace( /^#/, '' ) || 'blank' ) + '.';
-
-        // Iterate over all nav links, setting the "selected" class as-appropriate.
-        $('#osa1 a').each(function(){
-        var that = $(this);
-        that[ that.attr( 'href' ) === hash ? 'addClass' : 'removeClass' ]( 'selected' );
-        });
-        })
-
-        // Since the event is only triggered when the hash changes, we need to trigger
-        // the event now, to handle the hash the page may have loaded with.
-        $(window).hashchange();
-
-        });
+        
 	</head>
 <body>
 <div id="header-wrapper">
