3a83855c34e4bd1eb22e94164ba77d9f2c7ac52a
silverwithp <silverpajumae@hotmail.com>
2015-04-28 11:05:49 +0300

    Kustutan scripti rea

--- index.php
+++ index.php
@@ -79,7 +79,6 @@ setInterval(function () {
 
 <div class="clear"></div>
 
-<script type="text/javascript" src="countdown.js"></script>
 <script src="js/facebook.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
